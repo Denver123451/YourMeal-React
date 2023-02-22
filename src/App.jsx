b6e192757447e6705e33032836d8880fea9f2b85
Denver12345 <villaan12345>
2023-02-22 21:16:44 +0200

    add section & redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import {Container} from "./components/Container/Container";
+
 import {Header} from "./components/Header/Header";
+import {Navigation} from "./components/Navigation/Navigation";
+import {Catalog} from "./components/Catalog/Catalog";
+import {Provider} from "react-redux";
+import {store} from "./store/index.js";
 
 
 export const App = () => {
 
 
   return (
-      <>
+      <Provider store={store}>
           <Header />
           <main>
-              <nav>
-                  <Container className="navigation__container"/>
-              </nav>
-              <section></section>
+              <Navigation />
+              <Catalog />
           </main>
           <footer></footer>
-      </>
+      </Provider>
   )
 }
 
---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,44 @@
+import style from './Order.module.css';
+import {OrderGoods} from "../OrderGoods/OrderGoods";
+
+const orderList = ['Супер сырный', 'Картошка фри', 'Жгучий хот-дог'];
+
+export const Order = () => {
+
+
+
+
+    return(
+        <div className={style.order}>
+            <section className={style.order__wrapper}>
+                <div className={style.order__header} tabIndex="0" role="button">
+                    <h2 className={style.order__title}>Корзина</h2>
+
+                    <span className={style.order__count}>4</span>
+                </div>
+
+                <div className={style.order__wrap_list}>
+                    <ul className={style.order__list}>
+                        {orderList.map((item,i) => <OrderGoods key = {i} item = {item} />
+                        )}
+                    </ul>
+
+                    <div className={style.order__total}>
+                        <p>Итого</p>
+                        <p>
+                            <span className={style.order__amount}>1279</span>
+                            <span className="currency">₽</span>
+                        </p>
+                    </div>
+
+                    <button className={style.order__submit}>Оформить заказ</button>
+
+                    <div className={style.order__apeal}>
+                        <p className={style.order__text}>Бесплатная доставка</p>
+                        <button className={style.order__close}>Свернуть</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
